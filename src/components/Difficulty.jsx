c10fa89a56e70a7621dc035de37ae05ef14d3b64
Jaewon <jaewon.h4@gmail.com>
2023-11-15 11:41:20 -0800

    First commit

---
+++ src/components/Difficulty.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import BackIcon from "./icons/BackIcon"
+import "../styles/Difficulty.css"
+
+
+function Difficulty() {
+  return (
+    <div className="difficulty-page">
+      <header className="difficulty-header">
+        <BackIcon />
+        <div className="difficulty-title-container">
+          <span className="difficulty-title">
+            Difficulty
+          </span>
+        </div>
+      </header>
+      <div className="difficulty-container">
+        <Link to="/gameplay" className="difficulty easy">Easy</Link>
+        <Link to="/gameplay" className="difficulty medium">Medium</Link>
+        <Link to="/gameplay" className="difficulty hard">Hard</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Difficulty
