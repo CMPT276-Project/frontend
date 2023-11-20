4eeeb1e41e6515977ad64261420e43e85cb80be9
amaan-on-compsci <amaankhanx16@gmail.com>
2023-11-20 01:54:38 -0800

    Added collision check with map, updated code formatting

--- index.js
+++ index.js
@@ -1,54 +1,51 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
 class Sprite {
-    constructor(position) {
-        this.position = position
-        this.image = new Image()
-    }
+  constructor(position) {
+    this.position = position;
+    this.image = new Image();
+  }
 }
 
-
 //call constructor in Player.js file
-const player = new Player()
+const player = new Player();
 
 // Set default for keys
 const keys = {
-    w: {
-        pressed: false
-    },
-    ArrowUp: {
-        pressed: false
-    },
-    d: {
-        pressed: false
-    },
-    ArrowRight: {
-        pressed: false
-    },
-    s: {
-        pressed: false
-    },
-    ArrowLeft: {
-        pressed: false
-    } 
+  w: {
+    pressed: false,
+  },
+  ArrowUp: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  ArrowLeft: {
+    pressed: false,
+  },
+};
 
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.velocity.x = 0;
+
+  if (keys.d.pressed || keys.ArrowRight.pressed) player.velocity.x = 5;
+  else if (keys.s.pressed || keys.ArrowLeft.pressed) player.velocity.x = -5;
+  player.draw();
+  player.update();
 }
-
-function animate(){
-    window.requestAnimationFrame(animate)
-    c.fillStyle = 'white'
-    c.fillRect(0,0,canvas.width, canvas.height)
-
-    player.velocity.x = 0
-
-    if ((keys.d.pressed)||(keys.ArrowRight.pressed)) player.velocity.x = 5
-    else if ((keys.s.pressed)||(keys.ArrowLeft.pressed)) player.velocity.x = -5
-    player.draw()
-    player.update()
-}
-animate()
-
+animate();
--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,37 +1,47 @@
-class Player{
-    constructor(){
-        this.position = {
-            x: 100,
-            y: 100
-        }
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
 
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
 
-        this.width = 100
-        this.height = 100
-        this.sides = {
-            bottom: this.position.y + this.height
-        }
+    this.width = 75;
+    this.height = 75;
+    this.sides = {
+      bottom: this.position.y + this.height,
+    };
 
-        this.gravity = 1
+    this.gravity = 1;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.sides.bottom = this.position.y + this.height;
+
+    //above bottom of canvas
+    if (this.sides.bottom + this.velocity.y < canvas.height) {
+      this.velocity.y += this.gravity;
+    } else this.velocity.y = 0;
+
+    // collision check with the map
+    const col = Math.floor(this.position.x / this.width);
+    const row = Math.floor(this.position.y / this.height);
+    const index = row * 32 + col;
+
+    if (mapCollision[index] === 1) {
+      this.position.y = row * this.height;
+      this.velocity.y = 0;
     }
-
-    draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x,this.position.y,this.width,this.height)
-    }
-
-    update(){
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        this.sides.bottom = this.position.y + this.height
-
-        //above bottom of canvas
-        if(this.sides.bottom + this.velocity.y < canvas.height){
-            this.velocity.y += this.gravity
-        } else this.velocity.y = 0
-    }
+  }
 }
