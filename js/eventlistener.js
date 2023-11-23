6004312bfd47f97581b91c15e04e413cc933a795
Jaewon <jaewon.h4@gmail.com>
2023-11-23 02:00:08 -0800

    Implemented everything in the front page of the frontend

---
+++ js/eventlistener.js
@@ -0,0 +1,39 @@
+//Event Listener for Pushing Keydown
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    // move player up
+    case "w":
+    case " ":
+    case "ArrowUp":
+      if (player.velocity.y === 0) {
+        player.velocity.y = -15;
+        break;
+      }
+    //move player to the right
+    case "d":
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;
+      break;
+    //move player to the left
+    case "a":
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      break;
+  }
+});
+
+//Event listener for letting go of key, i.e velocity = 0
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    //move player to the right
+    case "d":
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+    //move player to the left
+    case "a":
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+      break;
+  }
+});
