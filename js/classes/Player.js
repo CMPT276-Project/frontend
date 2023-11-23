2c3258d4367ab53a1d6daf1cb0b315a9a39466e6
Sonia Parmar <sonia_parmar17@icloud.com>
2023-11-23 00:25:50 -0800

    collision detection complete

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -3,8 +3,8 @@ class Player{
         collisionBlocks = []
     }){
         this.position = {
-            x: 100,
-            y: 100
+            x: 200,
+            y: 200
         }
 
         this.velocity = {
@@ -12,8 +12,8 @@ class Player{
             y: 0
         }
 
-        this.width = 100
-        this.height = 100
+        this.width = 25
+        this.height = 25
         this.sides = {
             bottom: this.position.y + this.height
         }
@@ -30,48 +30,55 @@ class Player{
     }
 
     update(){
-        this.position.x += this.velocity.x
-        //check for collisions in x direction
-        for (let i=0; i<this.collisionBlocks.length; i++){
-            const collisionBlock = this.collisionBlocks[i]
+            this.position.x += this.velocity.x
+            //check for collisions in x direction
+            for (let i=0; i<this.collisionBlocks.length; i++){
+                const collisionBlock = this.collisionBlocks[i]
 
-            //if there is a collision
-            if(this.position.x <= collisionBlock.position.x + collisionBlock.width &&
-                this.position.x + this.width >= collisionBlock.position.x &&
-                this.position.y + this.height >= collisionBlock.position.y &&
-                this.position.y <= collisionBlock.position.y + collisionBlock.height){
-                
-                    //collision on x-axis goign left
-                    if(this.velocity.x < -1) {
-                        this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01
-                        break
-                    }
-                    //collision on x-axis going right
-                    if(this.velocity.x > 1) {
-                        this.position.x = collisionBlock.position.x - this.width - 0.01
-                        break
-                    }
+                //if there is a collision
+                if(this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                    this.position.x + this.width >= collisionBlock.position.x &&
+                    this.position.y + this.height >= collisionBlock.position.y &&
+                    this.position.y <= collisionBlock.position.y + collisionBlock.height){
+                    
+                        //collision on x-axis goign left
+                        if(this.velocity.x < 0) {
+                            this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01
+                            break
+                        }
+                        //collision on x-axis going right
+                        if(this.velocity.x > 0) {
+                            this.position.x = collisionBlock.position.x - this.width - 0.01
+                            break
+                        }
+                } 
+            }
 
-                     //collision on x-axis goign left
-                    if(this.velocity.x < -1) {
-                        this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01
-                        break
-                    }
-                    //collision on x-axis going right
-                    if(this.velocity.x > 1) {
-                        this.position.x = collisionBlock.position.x - this.width - 0.01
-                        break
-                    }
-
-            } 
-        }
-
-        this.position.y += this.velocity.y
-        this.sides.bottom = this.position.y + this.height
-
-        //above bottom of canvas
-        if(this.sides.bottom + this.velocity.y < canvas.height){
+            //apply gravity
             this.velocity.y += this.gravity
-        } else this.velocity.y = 0
+            this.position.y += this.velocity.y
+            this.sides.bottom = this.position.y + this.height
+
+            for (let i=0; i<this.collisionBlocks.length; i++){
+                const collisionBlock = this.collisionBlocks[i]
+
+                //if there is a collision
+                if(this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                    this.position.x + this.width >= collisionBlock.position.x &&
+                    this.position.y + this.height >= collisionBlock.position.y &&
+                    this.position.y <= collisionBlock.position.y + collisionBlock.height){
+                    
+                        if(this.velocity.y < 0) {
+                            this.velocity.y = 0
+                            this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01
+                            break
+                        }
+                        if(this.velocity.y > 0) {
+                            this.velocity.y = 0
+                            this.position.y = collisionBlock.position.y - this.height - 0.01
+                            break
+                        }
+                } 
+            }
     }
 }
